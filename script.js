c6df1285077ec5b38922e94bba0fbc1e4e31b640
oskar1823 <62386364+oskar1823@users.noreply.github.com>
2024-04-11 20:47:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const modalOverlay = document.getElementById("modalOverlay");
+        const closeModal = document.getElementById("closeModal");
+
+        modalOverlay.style.display = "flex";
+
+        closeModal.addEventListener("click", function() {
+            modalOverlay.style.display = "none";
+        });
+    });
